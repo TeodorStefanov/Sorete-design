b03196878e1aa26a88d01ac6866b16261b4c5920
Teodor <teodor2@abv.bg>
2022-11-12 22:28:39 +0200

    cartPageEdit

--- project/src/pages/CartPage/index.js
+++ project/src/pages/CartPage/index.js
@@ -22,11 +22,20 @@ const CartPage = () => {
   }, []);
   return (
     <PageWrapper>
-      {products.length> 0 ? (
+      {products.length > 0 ? (
         <div className={styles.container}>
           <div className={styles.left}>
             <p className={styles.cartSummary}>Cart Summary</p>
-            <div className={styles.item}></div>
+            <div className={styles.item}>
+              <img className={styles.picture} src={products[0].imageUrl}></img>
+              <h1 className={styles.name}>{products[0].name}</h1>
+              <ol className={styles.price}>
+                <li>L: {quantity[0].L}</li>
+                <li>M: {quantity[0].M}</li>
+                <li>S: {quantity[0].S}</li>
+              </ol>
+              <p>Price: {products[0].price}</p>
+            </div>
           </div>
           <div className={styles.right}>
             <p>{products[0].description}</p>
