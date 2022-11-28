e561ed6b7a3755050909f339b960b1374df08dee
Teodor <teodor2@abv.bg>
2022-11-28 14:31:04 +0200

    feat/mainPage update

--- project/src/pages/HomePage/index.js
+++ project/src/pages/HomePage/index.js
@@ -7,7 +7,8 @@ import ItemMenu from "../../Components/Main/itemMenu";
 import Items from "../../Components/Main/items";
 import PageWrapper from "../../Components/page-wrapper";
 import styles from "./index.module.css";
-
+import pictureOne from "../../images/bottomPicture/block1_4.jpg";
+import pictureTwo from "../../images/bottomPicture/block2_4.jpg";
 const HomePage = () => {
   return (
     <PageWrapper>
@@ -19,8 +20,22 @@ const HomePage = () => {
       <BestItems />
       <ItemMenu />
       <ConceptMenu />
-      <BottomPictures/>
-      <div className={styles.bottom}></div>
+      <BottomPictures />
+      <div className={styles.pictures}>
+        <div className={styles.left}>
+          <img src={pictureOne} className={styles.pictureOne} />
+          <div className={styles.leftMiddle}>
+            <p>CELEBRATE YOUR BATHROOM WITH US</p>
+            <h2>Sorete Bath Fashion</h2>
+            <p>
+              Our philosophy is simple: we want our products to coordinate with
+              each other, to mix and match allowing our customers to create a
+              unique and personalized environment.
+            </p>
+          </div>
+        </div>
+        <img src={pictureTwo} className={styles.pictureTwo} />
+      </div>
     </PageWrapper>
   );
 };
