8116b6bae6a774846e079ac7d1c4888b818d65bb
Teodor <teodor2@abv.bg>
2022-12-17 19:25:35 +0200

    footer page repair

--- project/src/Components/Footer/footer/index.js
+++ project/src/Components/Footer/footer/index.js
@@ -1,4 +1,8 @@
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styles from "./index.module.css";
@@ -6,14 +10,24 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerText}>
-        <h1>Контакти</h1>
-        <p>България, град Варна</p>
+        <section className={styles.icons}>
+          <a href="https://www.facebook.com">
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a href="https://www.twitter.com">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a href="https://www.instagram.com">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+          <a href="https://www.linkedin.com">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a href="https://www.youtube.com">
+            <FontAwesomeIcon icon={faYoutube} size="2x" />
+          </a>
+        </section>
       </div>
-      <section className={styles.a}>
-        <a href="https://www.youtube.com">
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-      </section>
     </footer>
   );
 };
